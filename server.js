731cbac75cb14ccb277530056d94d5a299901849
enszrlu <enes.zrl@gmail.com>
2022-03-23 23:31:01 +0300

    socket introduced

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);;
+const cookieParser = require('cookie-parser');
+
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+
 //set view engine to pug
 app.set('view engine', 'pug')
 
@@ -31,7 +39,9 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  key: 'express.sid',
+  store: store
 }));
 
 // Use Flash
@@ -40,6 +50,18 @@ app.use(flash());
 //Use Passport
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
@@ -53,6 +75,7 @@ myDB(async client => {
     ++currentUsers;
     io.emit('user count', currentUsers);
     console.log('A user has connected');
+    console.log((socket.request.user.username || socket.request.user.name) + " has connected.");
 
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
@@ -73,3 +96,16 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
